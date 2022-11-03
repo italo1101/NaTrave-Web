2a6b0c84a37199c47ac7fef9402d447ee98f53b1
Italo Cruz <italocruz544@gmail.com>
2022-11-03 05:48:00 -0300

    solve error in the home bg

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -9,7 +9,7 @@ export function Home() {
   }
   
   return (  
-    <div className="h-screen bg-red-700 text-white p-4 flex flex-col items-center space-y-6">
+    <div className="min-h-screen bg-red-700 text-white p-4 flex flex-col items-center space-y-6">
       
       <header className="container flex justify-center max-w-5xl p-4">
         <img src="/image/logo-vinho.svg" className="w-40"></img>
